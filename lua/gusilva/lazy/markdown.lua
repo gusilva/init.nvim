afefcc3d8a03622df8ff534bcea4c9373cc29a1b
Gustavo Silva <silvagustavo@yandex.ru>
2024-10-16 13:10:43 +0200

    update plugin

--- lua/gusilva/lazy/markdown.lua
+++ lua/gusilva/lazy/markdown.lua
@@ -1,41 +1,58 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = "markdown",
-	-- build = function()
-	-- 	vim.fn["mkdp#util#install"]()
-	-- end,
-	build = function(plugin)
-		if vim.fn.executable("npx") then
-			vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-		else
-			vim.cmd([[Lazy load markdown-preview.nvim]])
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+		config = function()
+			require("render-markdown").setup({
+				latex = { enabled = false },
+			})
+		end,
+	},
+	-- {
+	-- 	"tadmccorkle/markdown.nvim",
+	-- 	ft = "markdown", -- or 'event = "VeryLazy"'
+	-- 	opts = {
+	-- 		-- configuration here or empty for defaults
+	-- 	},
+	-- },
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = "markdown",
+		build = function()
 			vim.fn["mkdp#util#install"]()
-		end
-	end,
-	config = function()
-		-- vim.fn["mkdp#util#install"]()
-		vim.keymap.set("n", "<leader>m", "<CMD>MarkdownPreview<CR>")
-		vim.keymap.set("n", "<leader>mn", "<CMD>MarkdownPreviewStop<CR>")
-		vim.g.mkdp_markdown_css = "~/markdown.css"
-		vim.g.mkdp_highlight_css = ""
-		vim.g.mkdp_port = "7777"
+		end,
+		config = function()
+			-- vim.fn["mkdp#util#install"]()
+			vim.keymap.set("n", "<leader>m", "<CMD>MarkdownPreview<CR>")
+			vim.keymap.set("n", "<leader>mn", "<CMD>MarkdownPreviewStop<CR>")
+			vim.g.mkdp_markdown_css = "~/markdown.css"
+			vim.g.mkdp_highlight_css = ""
+			vim.g.mkdp_port = "7777"
 
-		local mkdp_preview_options = {
-			mkit = {},
-			katex = {},
-			uml = {},
-			maid = {},
-			disable_sync_scroll = 0,
-			sync_scroll_type = "middle",
-			hide_yaml_meta = 1,
-			sequence_diagrams = {},
-			flowchart_diagrams = {},
-			content_editable = true,
-			disable_filename = 1,
-			toc = {},
-		}
+			local mkdp_preview_options = {
+				mkit = {},
+				katex = {},
+				uml = {},
+				maid = {},
+				disable_sync_scroll = 0,
+				sync_scroll_type = "middle",
+				hide_yaml_meta = 1,
+				sequence_diagrams = {},
+				flowchart_diagrams = {},
+				content_editable = true,
+				disable_filename = 1,
+				toc = {},
+			}
 
-		vim.g.mkdp_preview_options = mkdp_preview_options
-	end,
+			vim.g.mkdp_preview_options = mkdp_preview_options
+			-- open in google chrome
+			-- let g:mkdp_browser = '/usr/bin/google-chrome-stable'
+		end,
+	},
 }
--- lua/gusilva/lazy/which-key.lua
+++ lua/gusilva/lazy/which-key.lua
@@ -6,18 +6,15 @@ return {
 		require("which-key").setup()
 
 		-- Document existing key chains
-		-- require("which-key").register({
-		-- 	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-		-- 	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-		-- 	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-		-- 	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-		-- 	["<leader>W"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-		-- 	["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-		-- 	["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-		-- })
-		-- visual mode
-		-- require("which-key").register({
-		-- 	["<leader>h"] = { "Git [H]unk" },
-		-- }, { mode = "v" })
+		require("which-key").add({
+			{ "<leader>W", desc = "[W]orkspace" },
+			{ "<leader>c", desc = "[C]ode" },
+			{ "<leader>d", desc = "[D]ocument" },
+			{ "<leader>r", desc = "[R]ename" },
+			{ "<leader>s", desc = "[S]earch" },
+			{ "<leader>t", desc = "[T]oggle" },
+			{ "<leader>h", desc = "Git [H]unk" },
+			{ "<leader>h", desc = "Git [H]unk", mode = "v" },
+		})
 	end,
 }
